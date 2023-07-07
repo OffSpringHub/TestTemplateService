edaa3c6f0a55ac968b595cf3201cda78422539f6
anasmoaya <anassmoayaa@gmail.com>
2023-07-07 17:30:12 +0100

    first commit : envir created

---
+++ src/test/java/com/offshoringhub/testtemplateserver/TestTemplateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offshoringhub.testtemplateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTemplateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
